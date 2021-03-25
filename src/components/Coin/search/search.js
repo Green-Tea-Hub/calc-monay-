1bbf51bc09c346aea9b57928d6a69e73b9550777
Alexander <iaks.fine@gmail.com>
2021-03-25 11:48:16 +0300

    Add search func

---
+++ src/components/Coin/search/search.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * let index = getSearchId([1,8,9,"haha"], "8");
+ * @param {Array} db
+ * @param {*} target
+ * @returns index number or false
+ */
+function getSearchId(db, target) {
+  let l = 0; // left
+  let r = db.length - 1; // right
+  let m; // mid
+
+  while (l <= r) {
+    m = Math.round((r - l) / 2) + l;
+
+    // if (~db[m][obj_name].indexOf(target)) return m !!! so heavy
+    if (target === db[m]) return m;
+    else if (target < db[m]) r = m - 1;
+    else l = m + 1;
+  }
+
+  return false;
+}
