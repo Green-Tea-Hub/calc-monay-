709a1953e18798d5c43104b96074f3e9898d8dbf
Alexander <iaks.fine@gmail.com>
2021-03-25 13:04:37 +0300

    I did it, add search function

--- src/components/Coin/coin.js
+++ src/components/Coin/coin.js
@@ -25,5 +25,12 @@ function get_CoinGecko(res) {
 
 const $Coin = document.getElementById("coin");
 
+// Add heandler to input "More" coins
 const input_more = $Coin.querySelector(".coins__input-more");
 input_more.addEventListener("click", createCoinsList, false);
+
+// Add heandler to search inputs
+["id", "symbol", "name"].forEach((placeholder) => {
+  const input = $Coin.querySelector(`input[placeholder="${placeholder}"]`);
+  input.addEventListener("change", searchInCoinsList, false);
+});
--- src/components/Coin/heandlers/searchInCoinsList.js
+++ src/components/Coin/heandlers/searchInCoinsList.js
@@ -1,11 +1,8 @@
 "use strict";
 
-async function searchInCoinsList() {
-  // Disabled Input
-  this.setAttribute("disabled", true);
-
+async function __searchInCoinsList() {
   const tag = this.placeholder;
-  // Checking data
+
   let coinsList;
   if (sessionStorage.coinsList) {
     coinsList = JSON.parse(sessionStorage.coinsList);
@@ -14,9 +11,9 @@ async function searchInCoinsList() {
     sessionStorage.coinsList = JSON.stringify(coinsList);
   }
 
-  const list_cont = $Coin.querySelector(".coins__list");
-  // "Clear History"
-  list_cont.innerHTML = "";
+  const list_cont =
+    // "Clear History"
+    (list_cont.innerHTML = "");
   input_more.innerText = 0;
 
   // Serach func
@@ -36,17 +33,6 @@ async function searchInCoinsList() {
     return false;
   };
 
-  coinsList.sort(function (a, b) {
-    if (a[tag] > b[tag]) {
-      return 1;
-    }
-    if (a[tag] < b[tag]) {
-      return -1;
-    }
-    // a должно быть равным b
-    return 0;
-  });
-
   const search_answer_id = getSearchId(
     coinsList,
     this.innerText.toLocaleLowerCase()
@@ -62,3 +48,65 @@ async function searchInCoinsList() {
   // Enable Input
   this.removeAttribute("disabled");
 }
+
+async function searchInCoinsList() {
+  // Get Coins List
+  let coinsList;
+  if (sessionStorage.coinsList) {
+    coinsList = JSON.parse(sessionStorage.coinsList);
+  } else {
+    coinsList = await get_CoinGecko("coins/list");
+    sessionStorage.coinsList = JSON.stringify(coinsList);
+  }
+
+  // Search func
+  function getSearchId(db, target, tag) {
+    let l = 0; // left
+    let r = db.length - 1; // right
+    let m; // mid
+
+    while (l <= r) {
+      m = Math.round((r - l) / 2) + l;
+
+      // if (~db[m][obj_name].indexOf(target)) return m !!! so heavy
+      if (target === db[m][tag]) return m;
+      else if (target < db[m][tag]) r = m - 1;
+      else l = m + 1;
+    }
+
+    return false;
+  }
+
+  const tag = this.placeholder;
+
+  // Sort tags
+  coinsList = coinsList.sort(function (a, b) {
+    if (a[tag] > b[tag]) {
+      return 1;
+    }
+    if (a[tag] < b[tag]) {
+      return -1;
+    }
+    // a должно быть равным b
+    return 0;
+  });
+
+  const target = this.value.replace(/ +/g, " ").trim().toLocaleLowerCase();
+  const search_mass_id = getSearchId(coinsList, target, tag);
+
+  if (search_mass_id) {
+    const fk_answer = [];
+    fk_answer.push(coinsList[search_mass_id]);
+
+    $Coin.querySelector(".coins__list").innerHTML = "";
+    input_more.innerText = 0;
+    addInCoinsList(fk_answer);
+  } else {
+    err("oh shit im sorry\n but this is " + search_mass_id);
+  }
+}
+
+// let cities = [{ id: 121, name: 'г. Урюпинск' }, { id: 122, name: 'г. Париж' }, { id: 123, name: 'г. Москва' }, { id: 124, name: 'г. Штормград' }];
+// let searchTerm = 'г. Москва';
+// let cityId = cities.find(city => city.name === searchTerm).id
+// console.log(cityId);
