0111b3ce6710834fe05a5b3af9a095b46a617b3d
Alexander <iaks.fine@gmail.com>
2021-03-27 00:25:12 +0300

    Clear function remove some code add more comments

--- src/components/Coin/heandlers/searchInCoinsList.js
+++ src/components/Coin/heandlers/searchInCoinsList.js
@@ -1,58 +1,11 @@
 "use strict";
 
-async function __searchInCoinsList() {
-  const tag = this.placeholder;
-
-  // Check sessionStorage
-  let coinsList;
-  if (sessionStorage.coinsList) {
-    coinsList = JSON.parse(sessionStorage.coinsList);
-  } else {
-    coinsList = await get_CoinGecko("coins/list");
-    sessionStorage.coinsList = JSON.stringify(coinsList);
-  }
-
-  const list_cont =
-    // "Clear History"
-    (list_cont.innerHTML = "");
-  input_more.innerText = 0;
-
-  // Serach func
-  const getSearchId = (db, target) => {
-    let l = 0; // left
-    let r = db.length - 1; // right
-    let m; // mid
-
-    while (l <= r) {
-      m = Math.round((r - l) / 2) + l;
-
-      if (target === db[m][tag]) return m;
-      else if (target < db[m][tag]) r = m - 1;
-      else l = m + 1;
-    }
-
-    return false;
-  };
-
-  const search_answer_id = getSearchId(
-    coinsList,
-    this.innerText.toLocaleLowerCase()
-  );
-
-  if (search_answer_id) {
-    const search_answer_db = coinsList[search_answer_id];
-    addInCoinsList(search_answer_db);
-  } else {
-    alert("oh shit im sorry");
-  }
-
-  // Enable Input
-  this.removeAttribute("disabled");
-}
-
+/** .coins__search-inps heandler
+ */
 async function searchInCoinsList() {
-  // Disabled Input
+  // Disabled Input and Start timer
   this.setAttribute("disabled", true);
+  console.time("Search coin time");
 
   // Get Coins List
   let coinsList;
@@ -63,35 +16,20 @@ async function searchInCoinsList() {
     sessionStorage.coinsList = JSON.stringify(coinsList);
   }
 
-  const tag = this.placeholder;
+  // Get ID - answer
+  const ID = getSearchId(
+    sortBit(coinsList, this.placeholder), // Get with tag sorted db
+    this.value.trim(), // Get "target" without " "
+    this.placeholder // Tag ...
+  );
 
-  // !!! CHANGE !!!
-  // Sort tags
-  coinsList = coinsList.sort(function (a, b) {
-    if (a[tag] > b[tag]) {
-      return 1;
-    }
-    if (a[tag] < b[tag]) {
-      return -1;
-    }
-    // a должно быть равным b
-    return 0;
-  });
+  $Coin.querySelector(".coins__list").innerHTML = "";
+  input_more.innerText = 0;
 
-  // !!! CHANGE TARGET !!!
-  const target = this.value.replace(/ +/g, " ").trim();
-  const search_mass_id = getSearchId(coinsList, target, tag);
+  // Send "DB" and create list with answer of search
+  addInCoinsList([coinsList[ID]]);
 
-  // !!! CHANGE THIS ALL !!!
-  if (search_mass_id) {
-    const fk_answer = [];
-    fk_answer.push(coinsList[search_mass_id]);
-
-    $Coin.querySelector(".coins__list").innerHTML = "";
-    input_more.innerText = 0;
-    addInCoinsList(fk_answer);
-  } else console.log("Search is " + search_mass_id);
-
-  // Enable Input
+  // End of Heandler
+  console.timeEnd("Search coin time");
   this.removeAttribute("disabled");
 }
