385e7f86ec70c50bdd5c5093e4242e4ea0bfa03e
Alexander <iaks.fine@gmail.com>
2021-03-27 10:45:58 +0300

    Move API function

--- src/components/Coin/coin.js
+++ src/components/Coin/coin.js
@@ -9,21 +9,6 @@
 */
 
 /**
- * await get_CoinGecko("ping");
- *
- * @param {*} res response "ping", "coins/list", "events" ...
- * @returns {Promise} coingecko.com api answer
- */
-function get_CoinGecko(res) {
-  return fetch(`https://api.coingecko.com/api/v3/${res}`) //
-    .then((response) => response.json())
-    .catch((error) => err(error + "\nResponse: " + res));
-}
-// ["ping", "events"].forEach(async (res) =>
-//   console.log(await get_CoinGecko(res))
-// );
-
-/**
  * const a = sortBit([{tag},{tag},{tag}], tag);
  * @param {Array} db
  * @param {String} tag or symbol
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 "use strict";
 
-/** When error is fu . so mutch */
+/** When error
+ * @param {String} error_text
+ * => Alert reload window
+ */
 const err = (error_text) => {
   confirm(error_text + "\n\n This page will be reload")
     ? window.location.reload()
     : false;
 };
+
+/**
+ * await get_CoinGecko("ping");
+ *
+ * @param {*} res response "ping", "coins/list", "events" ...
+ * @returns {Promise} coingecko.com api answer
+ */
+function get_CoinGecko(res) {
+  return fetch(`https://api.coingecko.com/api/v3/${res}`) //
+    .then((response) => response.json())
+    .catch((error) => err(error + "\nResponse: " + res));
+}
+// ["ping", "events"].forEach(async (res) =>
+//   console.log(await get_CoinGecko(res))
+// );
