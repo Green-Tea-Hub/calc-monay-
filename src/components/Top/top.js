c660e55cf750c06a50fc36a31274c821eb46cac6
Alexander <iaks.fine@gmail.com>
2021-03-28 12:34:52 +0300

    Add sessStor and log timer

--- src/components/Top/top.js
+++ src/components/Top/top.js
@@ -11,21 +11,36 @@
 const $Top = document.getElementById("top");
 
 async function add_TP_onPage(place) {
-  const top_trending = await get_CoinGecko("search/trending");
+  console.time("Add Top Trending");
+
+  let top_trending;
+  if (sessionStorage.top_trending) {
+    top_trending = JSON.parse(sessionStorage.top_trending);
+  } else {
+    top_trending = await get_CoinGecko("search/trending");
+    sessionStorage.top_trending = JSON.stringify(top_trending);
+  }
 
   if (!top_trending) return;
 
   const text_block = document.createElement("p");
   text_block.classList = "top-trending__text";
-  text_block.innerHTML = "Market cap rank: ";
 
   top_trending.coins.forEach((coin) => {
     text_block.innerHTML += `
-      ${coin.item.name} (<b>${coin.item.symbol}</b>): ${coin.item.market_cap_rank} |
+      |
+      Market cap rank
+      ${coin.item.name}
+      <img src="${coin.item.thumb}" alt="${coin.item.symbol}">
+      (<b>${coin.item.symbol}</b>):
+      ${coin.item.market_cap_rank}
+      |
     `;
   });
 
   place.appendChild(text_block);
+
+  console.timeEnd("Add Top Trending");
 }
 
 add_TP_onPage($Top.querySelector(".top-trending"));
