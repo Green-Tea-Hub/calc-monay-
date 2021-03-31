85e339911cc312c9dbda4a93017064f1e7f68848
Alexander <iaks.fine@gmail.com>
2021-03-31 12:51:53 +0300

    Remove function clearstring and mini changes

--- src/components/Calc/calc.js
+++ src/components/Calc/calc.js
@@ -8,18 +8,16 @@
   Add inputs Heandlers
 */
 
-function clrStr(str) {
-  return str.toLocaleLowerCase().trim();
-}
-
 const $Calc = document.getElementById("calc");
 
 // Add heandler to inputs "switch" coins
-const switch_inputs = $Calc.querySelectorAll(".calc-boxes .calc__switch-input");
+const switch_inputs = $Calc.querySelectorAll(".calc__switch-input");
 switch_inputs[0].addEventListener("change", changeCalcCoin, false);
 
 // Add heandler to input coins value
-const cash_inputs = $Calc.querySelectorAll(".calc-boxes .calc__cash-value");
+const cash_inputs = $Calc.querySelectorAll(".calc__cash-value");
 cash_inputs[0].addEventListener("change", changeCalcValue, false);
 
 const cc_rate_divs = $Calc.querySelectorAll(".calc__cash-rate");
+
+changePrice();
