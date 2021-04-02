8ad79e9dce41ae0d3717fe9669144263ad57aee4
Alexander <iaks.fine@gmail.com>
2021-04-02 23:59:31 +0300

    Clear comments

--- src/components/Calc/add_change/addOptionInSelector.js
+++ src/components/Calc/add_change/addOptionInSelector.js
@@ -9,13 +9,5 @@ function addOptionsInSelector(selector, options) {
   for (var option in options) {
     const a = getOptionsGroup(options[option], selector.name, "name", option); // Send this coins and get optgroup with him
     selector.appendChild(a); // Add in selector
-
-    // if (selector.name == "id") {
-    //   const a = getOptionsGroup(options[option], "id", "name", option); // Send this coins and get optgroup with him
-    //   selector.appendChild(a); // Add in selector
-    // } else if (selector.name == "symbol") {
-    //   const a = getOptionsGroup(options[option], "symbol", "name", option);
-    //   selector.appendChild(a);
-    // }
   }
 }
