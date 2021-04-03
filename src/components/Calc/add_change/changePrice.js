0f23ded8fcf1337ae04023985da18c498d83f2c2
Alexander <iaks.fine@gmail.com>
2021-04-04 00:55:46 +0300

    Make function simple

--- src/components/Calc/add_change/changePrice.js
+++ src/components/Calc/add_change/changePrice.js
@@ -2,36 +2,39 @@
 
 // NEED REAL RESTRUCT !!!
 async function changePrice() {
-  const cS = (str) => str.toLocaleLowerCase().trim(); // Clear String
-  const mainCoin_id = cS(coin_selectors[0].value); // To string
-  const mainCoin_value = +coinValue_inputs[0].value; // To number
+  const inputs_coin = coin_selectors;
+  const inputs_coin_value = coin_valueInputs;
 
-  // if (switch_inputs.length !== cash_inputs.length) return;
-  for (let i = 1; i < coin_selectors.length; i++) {
-    const otherCoin_symbol = cS(coin_selectors[i].value);
-    const otherCoin_val_inp = coinValue_inputs[i];
-    let mainCoin_PricePlan; // Ohh.. need get prices
+  const main_coin_id = inputs_coin[0].value; // To string get ID
+  const main_coin_val = +inputs_coin_value[0].value; // To number get Value
+
+  for (let i = 1; i < inputs_coin.length; i++) {
+    const oth_coin_symb = inputs_coin[i].value;
+    const oth_coin_val_inp = inputs_coin_value[i];
 
     if (
-      sessionStorage[mainCoin_id] &&
-      JSON.parse(sessionStorage[mainCoin_id])[otherCoin_symbol]
+      !sessionStorage[main_coin_id] ||
+      !JSON.parse(sessionStorage[main_coin_id])[oth_coin_symb]
     )
-      mainCoin_PricePlan = JSON.parse(sessionStorage[mainCoin_id]);
-    else {
-      const coin_symbol_mass = []; // Create and add needed coin symbols
-      coin_selectors.forEach((input) => coin_symbol_mass.push(cS(input.value)));
+      sessionStorage[main_coin_id] = JSON.stringify(
+        await getSimplePrice(main_coin_id, inputs_coin)
+      );
 
-      // Get object with coin prices from server
-      const r = `${mainCoin_id}&vs_currencies=${coin_symbol_mass.join("%2C")}`;
-      const api_obj_answer = await get_CoinGecko("simple/price?ids=" + r);
+    const main_coin_PricePlan = JSON.parse(sessionStorage[main_coin_id]);
+    const price = main_coin_PricePlan[oth_coin_symb];
 
-      // Save server answer
-      sessionStorage[mainCoin_id] = JSON.stringify(api_obj_answer[mainCoin_id]);
-      mainCoin_PricePlan = JSON.parse(sessionStorage[mainCoin_id]);
-    }
-
-    const ctc = mainCoin_PricePlan[otherCoin_symbol]; // Get price coin to coin
-    otherCoin_val_inp.value = (mainCoin_value * ctc).toFixed(2); // Add all information on page
-    coinRate_divs[i].innerText = `${mainCoin_id} - ${ctc}`;
+    oth_coin_val_inp.value = (main_coin_val * price).toFixed(2); // Add all information on page
+    coinRate_divs[i].innerText = `1 ${main_coin_id} - ${price}`;
   }
+}
+
+async function getSimplePrice(main_coin_id, inputs_coin) {
+  const symb_mass = []; // Create and add needed coin symbols
+  inputs_coin.forEach((input) => symb_mass.push(input.value)); // Add inputs value in symb mass
+
+  const resp = `${main_coin_id}&vs_currencies=${symb_mass.join("%2C")}`;
+  const answer = await get_CoinGecko("simple/price?ids=" + resp);
+
+  // sessionStorage[main_coin_id] = JSON.stringify(answer[main_coin_id]); // Save server answer
+  return answer[main_coin_id]; // Add anwer in plan
 }
