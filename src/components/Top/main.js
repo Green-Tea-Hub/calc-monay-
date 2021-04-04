9b6d3f14a9360f89442c7fcce5203e4aada10d7a
Alexander <iaks.fine@gmail.com>
2021-04-05 00:33:24 +0300

    Make functin is simple AGAIN

--- src/components/Top/main.js
+++ src/components/Top/main.js
@@ -10,25 +10,25 @@
 
 const $Top = document.getElementById("top");
 
-(async (place) => {
-  // Get/ save coins
-  let top_trending_coins;
-  if (sessionStorage.top_trending_coins)
-    top_trending_coins = JSON.parse(sessionStorage.top_trending_coins);
-  else {
-    top_trending_coins = await get_CoinGecko("search/trending");
-    sessionStorage.top_trending_coins = JSON.stringify(top_trending_coins);
-  }
+(async () => {
+  if (!sessionStorage.top_trending_coins)
+    sessionStorage.top_trending_coins = JSON.stringify(
+      await get_CoinGecko("search/trending")
+    );
 
-  // Create text container
-  const txt_p = document.createElement("p");
-  txt_p.classList = "top__trending-text";
-  txt_p.innerHTML = "Top Trending (Market cap rank)";
-  top_trending_coins.coins.forEach((coin) => {
+  const tt_coins = JSON.parse(sessionStorage.top_trending_coins);
+  const tt_coins_p = document.createElement("p");
+
+  tt_coins_p.classList = "top__trending-text";
+  tt_coins_p.innerHTML = "Top Trending (Market cap rank): ";
+
+  const getSpan = (coin) => {
     const span = document.createElement("span");
-    span.innerHTML = `| ${coin.item.name} <img src="${coin.item.thumb}" alt="${coin.item.symbol}"> (<b>${coin.item.symbol}</b>): ${coin.item.market_cap_rank} |`;
-    txt_p.appendChild(span);
-  });
+    span.innerHTML = `${coin.item.name}: ${coin.item.market_cap_rank}<img src="${coin.item.thumb}"> | `;
+    return span;
+  };
 
-  place.appendChild(txt_p);
-})($Top.querySelector(".top__trending"));
+  tt_coins.coins.forEach((coin) => tt_coins_p.append(getSpan(coin)));
+
+  $Top.querySelector(".top__trending").appendChild(tt_coins_p);
+})();
